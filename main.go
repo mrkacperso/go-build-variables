178f135bb746926bb970abb58dc1b683ef7ae4b3
Kacper Kazimierak <kacper@kodeit.dev>
2020-10-20 17:20:25 +0200

    Created program structure

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const ProgramName = "go-build-variables"
+
+var (
+	version = "0.0.1"
+	build   = "000000"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Printf("%s usage:\n", ProgramName)
+		fmt.Printf("\t-v, --version\t print vesrsion and build\n")
+		return
+	}
+
+	arg := os.Args[1]
+	if arg == "-v" || arg == "--version\n" {
+		fmt.Printf("%s version %s, build: %s\n", ProgramName, version, build)
+	}
+}
