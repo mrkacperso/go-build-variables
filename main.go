97e45673a8691586896ce7d7754cbf3cc5f5d020
Kacper Kazimierak <kacper@kodeit.dev>
2020-10-20 19:08:07 +0200

    Simplify example

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
-
-const ProgramName = "go-build-variables"
 
 var (
 	Version  = "0.0.1"
@@ -13,14 +10,5 @@ var (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("%s usage:\n", ProgramName)
-		fmt.Printf("\t-v, --version\t print vesrsion and build\n")
-		return
-	}
-
-	arg := os.Args[1]
-	if arg == "-v" || arg == "--version\n" {
-		fmt.Printf("%s version %s, build: %s\n", ProgramName, Version, Build)
-	}
+	fmt.Printf("version %s, build: %s\n", Version, Build)
 }
